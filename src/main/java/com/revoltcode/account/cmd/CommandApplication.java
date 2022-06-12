c79b4d0f4ac00189fdf4c2518a78e3b0a9c1ffbd
oddy-bassey <oddybassey@gmail.com>
2022-06-12 19:43:32 +0100

    registered handlers

--- src/main/java/com/revoltcode/account/cmd/CommandApplication.java
+++ src/main/java/com/revoltcode/account/cmd/CommandApplication.java
@@ -1,13 +1,37 @@
 package com.revoltcode.account.cmd;
 
+import com.revoltcode.account.cmd.command.*;
+import com.revoltcode.account.cmd.infrastructure.handler.CommandHandler;
+import com.revoltcode.cqrs.core.infrastructure.dispatcher.CommandDispatcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+@RequiredArgsConstructor
 @SpringBootApplication
 public class CommandApplication {
 
+
+	@Autowired
+	private final CommandDispatcher commandDispatcher;
+
+	@Autowired
+	private final CommandHandler commandHandler;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
+	}
+
+	@PostConstruct
+	public void registerHandler(){
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(RestoreReadDbCommand.class, commandHandler::handle);
 	}
 
 }
